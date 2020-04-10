51c5b8c83a6339b2ddad6f5ce7fb7c25f894a8f0
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-10 14:40:09 -0500

    Personalizar productos

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from apps.products.models import ProductType, Product
 
+class ProductAdmin(admin.ModelAdmin):
+    # readonly_fields = ('created', 'updated') #No permite edicion de create y update
+    list_display = ('name', 'quantity', 'price', 'description', 'mark', 'product_types')
+    ordering = ('price','name','mark','product_types') # En caso que sea una sola ordering('column',)
+
+
 admin.site.register(ProductType)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
