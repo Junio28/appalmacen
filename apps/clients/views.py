93511de369cd5dc67b89aebb35b7dec39339a5ae
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-10 19:54:31 -0500

    Ajustar en apps/clients/views.py

--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 # Create your views here.
 from apps.clients.models import Client  #Se le incluye el modelo
@@ -15,7 +15,11 @@ class ClientCreate(CreateView):
    model = Client
    form_class = ClientForm
    template_name = 'clients/client_form.html'
-   success_url = reverse_lazy('cliente:client') 
+   success_url = reverse_lazy('cliente:client_list') 
+
+class ClientList(ListView):
+    model = Client
+    template_name = 'clients/client_list.html'
 
 
 
