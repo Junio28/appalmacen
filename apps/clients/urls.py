875b98c12ab8a05fe2869cf59f5e96b77a10c8e2
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-10 19:32:25 -0500

    Ajustar la ruta para el formulario

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from  apps.clients.views import index
+from  apps.clients.views import index, ClientCreate
+
+app_name = 'cliente'
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='client'),
+    path('nuevo/', ClientCreate.as_view(), name='client_new'),
 ]
