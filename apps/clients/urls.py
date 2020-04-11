acf5f6a3e72ac0becdc22d4cd9422ba2835b28c4
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-10 19:57:18 -0500

    Ajustar la ruta

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from  apps.clients.views import index, ClientCreate
+from  apps.clients.views import index, ClientCreate, ClientList
 
 app_name = 'cliente'
 
 urlpatterns = [
     path('', index, name='client'),
     path('nuevo/', ClientCreate.as_view(), name='client_new'),
+    path('listar/', ClientList.as_view(), name='client_list'),
 ]
