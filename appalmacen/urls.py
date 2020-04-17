b57b061dca0d5d0ca339d2905244667658babf7c
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-16 23:35:45 -0500

    Modificar la vista en Productos}

--- appalmacen/urls.py
+++ appalmacen/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cliente/', include('apps.clients.urls')),
+    path('producto/', include('apps.products.urls')),
 ]
